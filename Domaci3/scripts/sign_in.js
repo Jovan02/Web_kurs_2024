628ef034395d4a41f2884c0bb8e4955e9d750223
Jovan02 <jovannikolic2801@gmail.com>
2024-04-27 12:23:42 +0200

    Add login functionality

--- Domaci3/scripts/sign_in.js
+++ Domaci3/scripts/sign_in.js
@@ -3,31 +3,31 @@ let signInButton = document.getElementById("sign-in-button");
 signInButton.addEventListener("click", handleSignIn);
 
 async function handleSignIn() {
-  let username = document.getElementById("name").value;
-  let password = document.getElementById("password").value;
+    let username = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
 
-  let payloadData = {
-    username: username,
-    password: password,
-  };
+    let payloadData = {
+        username: username,
+        password: password,
+    };
+    console.log(payloadData);
 
-  try {
-    let results = await fetch("https://dev-lab.dev/api/login/", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payloadData),
-    });
+    try {
+        let results = await fetch("https://dev-lab.dev/api/login/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payloadData),
+        });
 
-    let data = await results.json();
-    console.log(data);
+        let data = await results.json();
 
-    if (data.success) {
-      localStorage.setItem("accessTtoken", data.token);
-      localStorage.setItem("refreshToken", username);
+        if (data.success) {
+            localStorage.setItem("accessTtoken", data.token);
+            localStorage.setItem("refreshToken", username);
+        }
+    } catch (err) {
+        console.log(err);
     }
-  } catch (err) {
-    console.log(err);
-  }
 }
